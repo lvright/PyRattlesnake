60ef1828127091d7d17a8cd91e5cb5549923797a
lvright <lvright@outlook.com>
2022-09-30 14:47:22 +0800

    完善菜单管理功能

--- backend/apis/common/menu.py
+++ backend/apis/common/menu.py
@@ -11,7 +11,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from typing import Optional
 
 from backend.core import setting, create_access_token, check_jwt_token, celery
-from backend.scheams import Result, Token, ChangeSort, ChangeStatus, DeleteIds, menu
+from backend.scheams import Result, Token, ChangeSort, ChangeStatus, DeleteIds, Menu
 from backend.models import UserMenu
 from backend.crud import CRUDBase, getMenu
 from backend.apis.deps import get_db, get_current_user, get_redis, page_total
@@ -45,3 +45,28 @@ async def get_page_dept(
     total = await getMenu.get_number(db)
     query_obj = {"name": name, "code": code, "hidden": hidden, "status": status, "maxDate": maxDate, "minDate": minDate}
     return resp_200(data=await getMenu.getQueryReclcle(db, pageIndex=page, pageSize=pageSize, query_obj=query_obj))
+
+@router.put(path="/system/menu/update/{id:path}", response_model=Result, summary="保存菜单")
+async def update_menu(id: int, menu: Menu, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
+    await getMenu.update(db, id, obj_in=menu.dict())
+    return resp_200(msg="保存成功")
+
+@router.post(path="/system/menu/save", response_model=Result, summary="添加菜单")
+async def save_menu(menu: Menu, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
+    await getMenu.create(db, obj_in=menu.dict())
+    return resp_200(msg="添加成功")
+
+@router.put(path="/system/menu/changeStatus", response_model=Result, summary="修改菜单状态")
+async def change_status_menu(menu: ChangeStatus, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
+    await getMenu.update(db, id=menu.id, obj_in={"status": menu.status})
+    return resp_200(msg="修改成功")
+
+@router.delete(path="/system/menu/delete", response_model=Result, summary="删除菜单[逻辑删除]")
+async def delete_menu(menu: DeleteIds, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
+    for id in menu.ids: await getMenu.tombstone(db, id)
+    return resp_200(msg="删除成功")
+
+@router.put(path="/system/menu/numberOperation", response_model=Result, summary="修改菜单列表排序")
+async def num_operation_dept(menu: ChangeSort, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
+    await getMenu.getChangeSort(db, obj_in=menu.dict())
+    return resp_200(msg="修改成功")
--- backend/scheams/menu.py
+++ backend/scheams/menu.py
@@ -6,14 +6,13 @@ from typing import Optional
 
 class Menu(BaseModel):
     """ 系统路由模型 """
-    code: Optional[str] = None
+    name: Optional[str] = None
     path: Optional[str] = None
-    parent_id: Optional[int] = None
+    parent_id: Optional[int] = 0
     icon: Optional[str] = None
     redirect: Optional[str] = None
-    name: Optional[str] = None
-    hidden: Optional[bool] = None
-    hiddenBreadcrumb: Optional[bool] = None
+    title: Optional[str] = None
+    hidden: Optional[str] = None
     type: Optional[str] = None
     component: Optional[str] = None
     sort: Optional[int] = None
