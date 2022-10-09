23e02e51152a2356f8bcaaed0679a4db6833878e
lvright <lvright@outlook.com>
2022-10-09 15:21:40 +0800

    过滤接口应用

--- backend/apis/api_router.py
+++ backend/apis/api_router.py
@@ -2,7 +2,7 @@
 
 from fastapi import APIRouter
 from backend.apis.common import (
-    login, admin, post, dept, role, system, dict
+    login, admin, post, dept, role, system, dict, menu
 )
 
 app_router = APIRouter()
