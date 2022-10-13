f005c5fc462a195c431708ac96b1357f2d27aa41
lvright <lvright@outlook.com>
2022-10-13 17:49:22 +0800

    删除监控服务

--- backend/apis/common/system.py
+++ backend/apis/common/system.py
@@ -17,21 +17,21 @@ from backend.db import MyRedis
 
 router = APIRouter()
 
-@router.get(path="/system/common/getLoginLogList", summary="系统登录日志")
+@router.get(path="/system/common/getLoginLogList", response_model=Result, summary="系统登录日志")
 async def get_login_log(username: str, orderBy: str, orderType: str, pageSize: int, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
     total = await getLoginLog.get_number(db)
     result = await getLoginLog.get_multi(db, orderBy="login_time", orderType="desc", pageIndex=1, pageSize=pageSize)
     result = [res for res in result if res["username"] == username and res]
     return resp_200(data={"items": result, "pageInfo": {"total": total, "currentPage": 1, "totalPage": page_total(total, pageSize)}})
 
-@router.get(path="/system/common/getOperationLogList", summary="系统登录日志")
+@router.get(path="/system/common/getOperationLogList", response_model=Result, summary="系统登录日志")
 async def get_oper_log(username: str, orderBy: str, orderType: str, pageSize: int, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
     total = await getOperLog.get_number(db)
     result = await getOperLog.get_multi(db, orderBy="login_time", orderType="desc", pageIndex=1, pageSize=pageSize)
     result = [res for res in result if res["username"] == username and result]
     return resp_200(data={"items": result, "pageInfo": {"total": total, "currentPage": 1, "totalPage": page_total(total, pageSize)}})
 
-@router.put(path="/system/setting/backendSetting", summary="更新系统配置")
+@router.put(path="/system/setting/backendSetting", response_model=Result, summary="更新系统配置")
 async def update_backend_setting(backend_setting: BackendSetting, db: AsyncSession = Depends(get_db), token: str = Depends(check_jwt_token)):
     await getBackendSetting.updateBackendSetting(db, user_id=token["id"], obj_in=backend_setting.dict())
     return resp_200(msg="更新成功")
