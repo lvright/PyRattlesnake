fe43a090968314239e7933d95646172faeb79985
lvright <lvright@outlook.com>
2022-10-26 15:18:07 +0800

    优化代码规范

--- backend/apis/common/login.py
+++ backend/apis/common/login.py
@@ -15,7 +15,7 @@ from backend.db import MyRedis
 router = APIRouter()
 
 
-@router.post(path='/system/login', response_model=Result, summary='后台登录')
+@router.post(path='/system/login', response_model=Result, summary="后台登录")
 async def login_access_token(request: Request, db: AsyncSession = Depends(get_db),
                              from_data: OAuth2PasswordRequestForm = Depends()):
     token = await toLogin.go(db, request, form_data={"username": from_data.username, "password": from_data.password})
